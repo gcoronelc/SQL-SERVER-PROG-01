a5ffbc3467d2423c8e0a97130a3aef21cb55b0ba
Gustavo Coronel <gcoronelc@gmail.com>
2020-11-13 17:13:47 -0500

    Repositorio

--- Unidad01/Work-Unidad-01.sql
+++ Unidad01/Work-Unidad-01.sql
@@ -21,28 +21,48 @@ GO
 ALTER INDEX [City] ON [dbo].[Customers] REBUILD
 GO
 
-
 select * from dbo.Customers
 where city like 'London';
 go
 
-SELECT a.index_id, name, avg_fragmentation_in_percent 
-FROM sys.dm_db_index_physical_stats (DB_ID(), 
-OBJECT_ID(N'dbo.Customers'), NULL, NULL, NULL) AS a
-JOIN sys.indexes AS b 
-ON a.object_id = b.object_id AND a.index_id = b.index_id;
+
+-- Recostruir indice
+
+ALTER INDEX PK_Customers ON DBO.Customers REBUILD;
 GO
+
+
+ALTER INDEX ALL ON DBO.Customers REBUILD;
+GO
+
+
+-- Fragmentación de índices
+
+
+SELECT 
+	c.name "Table name",
+	b.name "Index", 
+	avg_fragmentation_in_percent "Frag (%)", 
+	page_count "Page count" 
+FROM sys.dm_db_index_physical_stats (DB_ID(), 
+NULL, NULL, NULL, NULL ) AS a
+JOIN sys.indexes AS b 
+ON a.object_id = b.object_id AND a.index_id = b.index_id
+JOIN sys.tables c ON b.object_id = c.object_id
+ORDER BY  3 DESC;
+GO
+
+
 
 -- Para eliminar la fragmentación
 
-ALTER INDEX [PK_Customers] ON [dbo].[Customers] REORGANIZE  WITH ( LOB_COMPACTION = ON )
+ALTER INDEX [PK_Customers] ON [dbo].[Customers] REORGANIZE;
+GO
+
+ALTER INDEX ALL ON DBO.Customers REORGANIZE;
 GO
 
 
-ALTER INDEX PK_Customers 
-ON DBO.Customers
-REBUILD;
-GO
 
 
 
